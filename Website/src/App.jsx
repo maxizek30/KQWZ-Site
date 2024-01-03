681869ca199ba36264245947d890c2ceb22a0ff9
Max Lopez <familiacachorin@gmail.com>
2024-01-03 13:29:32 -0800

    create pages for react-router

--- Website/src/App.jsx
+++ Website/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import About from '../pages/About'
+import Home from '../pages/Home'
+import Contact from '../pages/Contact'
+import Stream from '../pages/Stream'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Router>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/stream" element={<Stream />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
   )
 }
