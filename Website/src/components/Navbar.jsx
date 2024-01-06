f04e7e390e7ed683c0ca1bdeb8b90c1a6a22cf92
Max Lopez <familiacachorin@gmail.com>
2024-01-05 16:12:32 -0800

    install mui icons and make navbar responsive

--- Website/src/components/Navbar.jsx
+++ Website/src/components/Navbar.jsx
@@ -1,23 +1,46 @@
-import { AppBar, Box, Button, Toolbar } from "@mui/material"
+import { AppBar, Box, Button, Toolbar, Divider, List, ListItem, ListItemButton, ListItemText, CssBaseline, IconButton, Drawer} from "@mui/material"
+import MenuIcon from '@mui/icons-material/Menu';
 import { Link, useLocation} from "react-router-dom"
 import logoUrl from '../assets/logo.jpg'
+import { useState } from "react";
 
 
 function Navbar() {
-  const location = useLocation(); // Hook to get the current location
-
-
   const links = [
     { path: "/", title: "Home" },
     { path: "/stream", title: "Stream" },
     { path: "/about", title: "About" },
     { path: "/contact", title: "Contact" }
   ];
+
+  //color
+  const location = useLocation(); // Hook to get the current location
   const isActive = (path) => location.pathname === path;
 
+  //drawer  
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen)
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Divider />
+      <List>
+        {links.map((link) => (
+          <ListItem key={link.title} disablePadding>
+            <ListItemButton component={Link} to={link.path}>
+              <ListItemText primary={link.title} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
     <>
+     <CssBaseline />
       <AppBar component="nav" sx={{ bgcolor: 'white' }}>
         <Toolbar>
           <Box sx={{ flexGrow: 1}}>
@@ -25,6 +48,7 @@ function Navbar() {
               <img src={logoUrl} alt="Logo" style={{ height: 50 }}/> 
             </Link>
           </Box>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
           {links.map((link, index) => (
             <Button 
               key={index} 
@@ -40,8 +64,31 @@ function Navbar() {
               {link.title}
             </Button>
           ))}
+          </Box>
+          <IconButton
+            color="black"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+            >
+              <MenuIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
+      <Drawer
+        anchor="top"
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{ keepMounted: true }}
+        sx={{
+          display: { xs: 'block', sm: 'none'},
+          '& .MuiDrawer-paper': { boxSizing: 'border-box' },
+        }}
+      >
+        {drawer}
+      </Drawer>
     </>
   )
 }
