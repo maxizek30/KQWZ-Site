7449b708374625f000092dbe25efc54888cce15a
Max Lopez <familiacachorin@gmail.com>
2024-01-05 18:24:03 -0800

    Create hero section

--- Website/src/pages/Home.jsx
+++ Website/src/pages/Home.jsx
@@ -1,6 +1,36 @@
+import { Grid, Paper, Typography, Button } from "@mui/material"
+import HeroImage from '../assets/backgroundImage.jpg'
+
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <Paper
+        sx={{
+          backgroundImage: `url(${HeroImage})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          height: '80vh',
+        }}
+      >
+        <Grid
+          container
+          alignItems="center"
+          sx={{ height: '100%' }}
+        >
+          <Grid item xs={12} sm={6} md={4}>
+            <Typography variant="h2" gutterBottom>
+              Welcome to Our Website
+            </Typography>
+            <Typography variant="h5" gutterBottom>
+              Explore our amazing products and services.
+            </Typography>
+            <Button variant="contained" color="primary" size="large">
+            Learn More
+            </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+    </>
   )
 }
 
