698149289f2effa9bdd488405162e87d7168c8e6
Max Lopez <familiacachorin@gmail.com>
2024-01-25 18:38:00 -0800

    add modal feature to imagelist

--- Website/src/pages/Stream.jsx
+++ Website/src/pages/Stream.jsx
@@ -1,10 +1,21 @@
-import { Box, Container, Grid, ImageList, ImageListItem, Typography } from "@mui/material"
+import { useState } from "react";
+import { Box, Container, Grid, ImageList, ImageListItem, Typography, Modal } from "@mui/material"
 import HeadphoneImage from '../assets/headphones.jpg';
 import RadioPlayer from "../components/RadioPlayer";
 import flyers from "../flyers.js"
-import { red } from "@mui/material/colors";
 
 function Stream() {
+  const [open, setOpen] = useState(false);
+  const [SelectedImg, setSelectedImg] = useState('');
+
+  const handleOpen = (imgSrc) => {
+    setSelectedImg(imgSrc);
+    setOpen(true);
+  };
+
+  const handleClose = () => setOpen(false);
+
+  
   return (
     <>
       {/* Header Section */}
@@ -38,9 +49,11 @@ function Stream() {
       >
         <RadioPlayer />  
       </Box>
+      
       {/* Flyer Section */}
+      <Container>
       <Box py={{ xs: 2, sm: 4, md: 10, lg: 20 }}>
-        <Typography variant="h2" color="secondary" py={4} textAlign={"center"}>Flyers</Typography>
+        <Typography variant="h2" py={4} textAlign={"center"} sx={{color: "tertiary.main"}}>Flyers</Typography>
         <ImageList 
         gap={12}
         sx={{
@@ -49,7 +62,7 @@ function Stream() {
           }}
         >
           {flyers.map((item) => (
-            <ImageListItem key={(item.location)}>
+            <ImageListItem key={(item.location)} onClick={() => handleOpen(item.location)} sx={{cursor: "pointer"}}>
               <img 
                 src={item.location}
               />
@@ -57,6 +70,29 @@ function Stream() {
           ))}
         </ImageList>
       </Box>
+      </Container>
+      {/* Image Modal */}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          boxShadow: 24,
+          outline: 'none',
+          maxWidth: '90%',
+          maxHeight: '90%',
+          overflow: 'auto'
+          }}
+        >
+          <img src={SelectedImg} style={{width: "100%"}} />
+        </Box>
+      </Modal>
     </>
   )
 }
