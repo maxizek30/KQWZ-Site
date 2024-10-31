aa3b28ad42acfbe5c763ab763e865cee6d2090f3
Max Lopez <familiacachorin@gmail.com>
2024-10-30 22:53:07 -0700

    fix stream

--- Website/src/components/RadioPlayer.jsx
+++ Website/src/components/RadioPlayer.jsx
@@ -1,61 +1,67 @@
-import { useState, useRef } from 'react';
-import '../styles/RadioPlayer.css';
+import { useState, useRef } from "react";
+import "../styles/RadioPlayer.css";
 
 function RadioPlayer() {
-    const [isPlaying, setIsPlaying] = useState(false);
-    const audioRef = useRef(new Audio("https://stream.kqwzradio.org/kqwz.mp3"));
-    const musicNotesRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(
+    new Audio("https://us3.internet-radio.com/proxy/maxizek30?mp=/auburn")
+  );
+  const musicNotesRef = useRef(null);
 
-    const togglePlay = () => {
-        setIsPlaying(!isPlaying);
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying);
 
-        if (isPlaying) {
-            musicNotesRef.current.style.display = "none";
-            audioRef.current.pause();
-        } else {
-            musicNotesRef.current.style.display = "flex";
-            audioRef.current.play();
-        }
-    };
+    if (isPlaying) {
+      musicNotesRef.current.style.display = "none";
+      audioRef.current.pause();
+    } else {
+      musicNotesRef.current.style.display = "flex";
+      audioRef.current.play();
+    }
+  };
   return (
     <center>
-    <div className="radioContainer">
-        <div className="musicNotes" ref={musicNotesRef} style={{ color: "white" }}>
-            ♪  
-            <div className="doubleNote">
-                ♫ 
-            </div>
-            ♪
+      <div className="radioContainer">
+        <div
+          className="musicNotes"
+          ref={musicNotesRef}
+          style={{ color: "white" }}
+        >
+          ♪<div className="doubleNote">♫</div>♪
         </div>
         <div className="handle">
-            <div className="handleBarLeft"></div>
-            <div className="handleCushion"></div>
-            <div className="handleBarRight"></div>
+          <div className="handleBarLeft"></div>
+          <div className="handleCushion"></div>
+          <div className="handleBarRight"></div>
         </div>
         <div className="radio">
-            <div className="radioTopSection">
-                <div className="radioButtonBig"></div>
-                <div className="radioButtonSmall"></div>
-                <div className="volumeSlider"></div>
-                <div className="radioButtonSmall"></div>
-                <div className="radioButtonBig"></div>
+          <div className="radioTopSection">
+            <div className="radioButtonBig"></div>
+            <div className="radioButtonSmall"></div>
+            <div className="volumeSlider"></div>
+            <div className="radioButtonSmall"></div>
+            <div className="radioButtonBig"></div>
+          </div>
+          <div className="radioBottomSection">
+            <div className="discPlayer">
+              <div className="disc"></div>
             </div>
-            <div className="radioBottomSection">
-                <div className="discPlayer">
-                    <div className="disc"></div>
-                </div>
-                <div className="playButtonContainer" onClick={togglePlay}>
-                    <div className={`playButtonLeft ${isPlaying ? 'active' : ''}`}></div>
-                    <div className={`playButtonRight ${isPlaying ? 'active' : ''}`}></div>
-                </div>
-                <div className="discPlayer">
-                    <div className="disc"></div>
-                </div>
+            <div className="playButtonContainer" onClick={togglePlay}>
+              <div
+                className={`playButtonLeft ${isPlaying ? "active" : ""}`}
+              ></div>
+              <div
+                className={`playButtonRight ${isPlaying ? "active" : ""}`}
+              ></div>
             </div>
+            <div className="discPlayer">
+              <div className="disc"></div>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
     </center>
-  )
+  );
 }
 
-export default RadioPlayer
+export default RadioPlayer;
